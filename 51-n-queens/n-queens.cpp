4d9a28c6c61a4c3fda34f98d3e8fc1da41f2f9f8
ankurkharb <112976506+ankurkharb@users.noreply.github.com>
2025-04-25 20:47:59 +0530

    Time: 3 ms (64.95%) | Memory: 10.1 MB (92.59%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,30 +1,34 @@
 class Solution {
-public:
-    bool isSafe(vector<string> &board,int row,int col, int n)
+    bool isSafe(vector<string> & board, int row,int col,int n)
     {
         //horizontal
         for(int j=0;j<n;j++)
         {
-            if(board[row][j]=='Q')return false;
+            if(board[row][j]=='Q')
+                return false;
         }
         //vertical
-        for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
         {
-            if(board[i][col]=='Q')return false;
+            if(board[j][col]=='Q')
+                return false;
         }
         //left diagonal
-        for(int i=row,j=col;i>=0&&j>=0;i--,j--)
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--)
         {
-            if(board[i][j]=='Q')return false;
+            if(board[i][j]=='Q')
+                return false;
         }
-        //right diagonal
-        for(int i=row,j=col;i>=0 && j<n;i--,j++)
+         for(int i=row,j=col;i>=0 && j<n;i--,j++)
         {
-            if(board[i][j]=='Q')return false;
+            if(board[i][j]=='Q')
+                return false;
         }
         return true;
+
     }
-    void nQueens(vector<string> &board,int row,int n ,vector<vector<string>> &ans)
+
+    void nQueen(vector<string> &board, int row,int n, vector<vector<string>> &ans)
     {
         if(row==n)
         {
@@ -33,19 +37,20 @@ public:
         }
         for(int j=0;j<n;j++)
         {
-            if(isSafe(board,row,j,n))
+            if(isSafe(board,row, j,n))
             {
                 board[row][j]='Q';
-                nQueens(board,row+1,n,ans);
+                nQueen(board,row+1,n,ans);
                 board[row][j]='.';
             }
         }
+       
     }
+public:
     vector<vector<string>> solveNQueens(int n) {
-
         vector<string> board(n,string(n,'.'));
         vector<vector<string>> ans;
-        nQueens(board,0,n,ans);
+        nQueen(board,0,n,ans);
         return ans;
     }
 };
